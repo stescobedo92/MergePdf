845c74f7aae91ea787f00263795cbf42f9ce259f
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-10-30 13:43:10 -0600

    Add user input for PDF file paths

--- MergePdf/Program.cs
+++ MergePdf/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+Console.WriteLine("Path to first PDF: ");
+string pdf1Path = Console.ReadLine();
+
+Console.WriteLine("Path to second PDF: ");
+string pdf2Path = Console.ReadLine();
