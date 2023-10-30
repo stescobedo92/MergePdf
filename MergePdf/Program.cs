95fa92cddbda225be54fa3b529bd30593004c707
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-10-30 13:45:16 -0600

    Merge PDF files into a new document

--- MergePdf/Program.cs
+++ MergePdf/Program.cs
@@ -6,3 +6,18 @@ string pdf1Path = Console.ReadLine();
 
 Console.WriteLine("Path to second PDF: ");
 string pdf2Path = Console.ReadLine();
+
+Console.WriteLine("Name of the new document: ");
+string outputPath = Console.ReadLine();
+
+using (FileStream fs = new FileStream(path: outputPath, FileMode.Create))
+{
+    Document document = new Document();
+    PdfSmartCopy copy = new PdfSmartCopy(document, fs);
+    document.Open();
+
+    AddPdfToDocument(pdf1Path, copy);
+    AddPdfToDocument(pdf2Path, copy);
+
+    document.Close();
+}
